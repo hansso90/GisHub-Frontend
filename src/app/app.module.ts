0cee4d13678174562233cad2f90df080c6e18d7c
Hans Lemkes <hans.lemkes@kadaster.nl>
2018-05-04 17:04:37 +0200

    first setup items in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { SearchComponent } from './search/search.component'
 import { MenuComponent } from './menu/menu.component'
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
 
 
 @NgModule({
   bootstrap:    [ AppComponent ],
-  declarations: [ AppComponent, SearchComponent, MenuComponent ],
+  declarations: [ AppComponent, SearchComponent, MenuComponent, ItemComponent ],
   imports:      [ BrowserModule, NgSemanticModule ]
 })
 export class AppModule {}
