210507afb00d8f99fcb8c5a3e8d3076df90d767d
Hans Lemkes <hans.lemkes@kadaster.nl>
2018-05-04 14:36:58 +0200

    first setup angular semantic-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgSemanticModule } from "ng-semantic";
 
+import { SearchComponent } from './search/search.component'
+import { MenuComponent } from './menu/menu.component'
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:    [ AppComponent ],
+  declarations: [ AppComponent, SearchComponent, MenuComponent ],
+  imports:      [ BrowserModule, NgSemanticModule ]
 })
-export class AppModule { }
+export class AppModule {}
+
